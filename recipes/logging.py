0743e1715a0ebc222019db5158a364f04f7d6c86
astromancer <hannes@saao.ac.za>
2020-08-24 00:29:11 +0200

    update docstrings, remove progress logger

--- recipes/logging.py
+++ recipes/logging.py
@@ -9,26 +9,27 @@ from recipes.oo import ClassProperty
 from recipes.decor.base import DecoratorBase
 
 # relative libs
-from .pprint.progressbar import ProgressBarBase
 
 
 @contextmanager
 def all_logging_disabled(highest_level=logging.CRITICAL):
+    """
+    A context manager that will prevent any logging messages triggered during
+    the body from being processed.
+    
+    Parameters
+    ----------
+    highest_level: int
+        The maximum logging level in use. This would only need to be changed if
+        a custom level greater than CRITICAL is defined.
+    """
     # source: https://gist.github.com/simon-weber/7853144
-    """
-    A context manager that will prevent any logging messages
-    triggered during the body from being processed.
-    :param highest_level: the maximum logging level in use.
-      This would only need to be changed if a custom level greater than CRITICAL
-      is defined.
-    """
     # two kind-of hacks here:
     #    * can't get the highest logging level in effect => delegate to the user
     #    * can't get the current module-level override => use an undocumented
     #       (but non-private!) interface
 
     previous_level = logging.root.manager.disable
-
     logging.disable(highest_level)
 
     try:
@@ -37,34 +38,32 @@ def all_logging_disabled(highest_level=logging.CRITICAL):
         logging.disable(previous_level)
 
 
-# class LoggingMixin():
-#     """
-#     Mixin class that exposes the `logger` attribute for the class.
-#     `logger` is an instance of logging.Logger
-#     """
-#
-#     show_module_depth = 1  # eg:. foo.sub.Klass #for depth of 2
-#
-#     def get_name(self):
-#         cls = type(self)
-#         parents = cls.__module__.split('.')
-#         parts = parents[:-self.show_module_depth - 1:-1] + [cls.__name__]
-#         name = '.'.join(filter(None, parts))
-#         return name
-#
-#     @property  # NOTE: making this a property avoids pickling error for the logger
-#     def logger(self):
-#         logger = logging.getLogger(self.name)
-#         return logger
+# FIXME: doesn't work! EG: 
+# from graphing.sliders import TripleSliders
+# TripleSliders.logger.setLevel(logging.DEBUG)
+# TripleSliders.logger.debug('nothing')  # no message :'(
 
 class LoggingMixin(object):
     """
-    Mixin class that exposes the `logger` attribute for the class.
-    `logger` is an instance of logging.Logger
+    Mixin class that exposes the `logger` attribute for the class which is an
+    instance of python's build in `logging.Logger`.  Allows for easy
+    customization of loggers on a class by class level.
+
+    Example
+    -------
+    # in sample.py
+    >>> class Sample(LoggingMixin):
+            def __init__(self):
+                self.logger.debug('Initializing')
+    
+    >>> from sample import Sample
+    >>> Sample.logger.setLevel(logging.debug)
     """
     _show_module_depth = 1  # eg:. foo.sub.Klass #for depth of 2
 
     # use `ClassProperty` decorator so we can access via cls.name and cls().name
+    # Making this attribute a property also avoids pickling errors since
+    # `logging.Logger` cannot be picked
     @ClassProperty
     @classmethod
     # @memoize
@@ -82,7 +81,6 @@ class LoggingMixin(object):
         return logging.getLogger(cls.log_name)
 
 
-# ====================================================================================================
 class catch_and_log(DecoratorBase, LoggingMixin):
     """
     Decorator that catches and logs errors instead of actively raising.
@@ -106,38 +104,7 @@ class catch_and_log(DecoratorBase, LoggingMixin):
             return result
         except Exception as err:
             self.logger.exception(
-                    '%s' % str(args))  # logs full trace by default
-
-
-class ProgressLogger(ProgressBarBase):
-    def __init__(self, precision=2, width=None, symbol='=', align='^',
-                 sides='|', every='2.5%', logname='progress'):
-        ProgressBarBase.__init__(self, precision, width, symbol, align, sides,
-                                 every)
-        self.name = logname
-
-    def update(self, i=None):
-        if i is None:
-            i = self.inc()
-
-        # don't update when unnecessary
-        if not self.needs_update(i):
-            return
-
-        # always update when state given
-        if i >= self.end:  # unless state beyond end
-            return
-
-        bar = self.get_bar(i)
-        logger = logging.getLogger(self.name)
-        logger.info('Progress: \n%s' % bar)
-
-# class SyncedProgressLogger(ProgressLogger):
-#     """can be used from multiple processes"""
-#      def __init__(self, counter, precision=2, width=None, symbol='=', align='^', sides='|',
-#                  logname='progress'):
-#          ProgressLogger.__init__(self, precision, width, symbol, align, sides, logname)
-#          self.counter = counter
+                '%s' % str(args))  # logs full trace by default
 
 
 # class MultilineIndenter(logging.LoggerAdapter):
