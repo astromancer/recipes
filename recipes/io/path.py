a5eae28c19ceba570fa696be3d1e3320feac7a10
astromancer <hannes@saao.ac.za>
2020-09-29 13:25:10 +0200

    improve filename matching with regex

--- recipes/io/path.py
+++ recipes/io/path.py
@@ -1,17 +1,39 @@
 import re
 import pathlib
+import operator as op
 
-class Path(type(pathlib.Path())):  #HACK!
-    def reglob(self, exp):
-        '''
-        glob.glob() style searching with regex
 
-        :param exp: Regex expression for filename
-        '''
-        m = re.compile(exp)
-        
-        names = map(lambda p: p.name, self.iterdir())
-        res = filter(m.match, names)
-        res = map(lambda p: self/p, res)
+def false(_):
+    return False
 
-        return res
+
+class Path(type(pathlib.Path())):  # HACK to get proper path type on all systems
+    def reglob(self, exp, ignore=None):
+        """
+        Filename matching with regular expressions
+
+        Parameters
+        ----------
+        exp : str
+            Regular expression for matching a filename
+        ignore : str or list, optional
+            Regular expression or list of filenames to ignore, by default None
+
+        Yields
+        -------
+        pathlib.Path
+            The matching paths
+        """
+        include = re.compile(exp)
+        exclude = false
+        if isinstance(ignore, (list, tuple)):
+            exclude = ignore.__contains__
+        elif isinstance(ignore, str):
+            exclude = re.compile(ignore).fullmatch
+        elif ignore:
+            raise TypeError('Ignore pattern or list is of invalid type')
+
+        for p in self.iterdir():
+            mo = include.fullmatch(p.name)
+            if mo and not exclude(p.name):
+                yield self / mo.group()
