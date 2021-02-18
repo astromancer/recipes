730250d08b9f96647020e5fb87dd5251848a8542
astromancer <hannes@saao.ac.za>
2021-02-18 10:14:56 +0200

    rename some functions, update docs

--- recipes/lists.py
+++ recipes/lists.py
@@ -3,6 +3,7 @@ Recipes involving lists
 """
 
 # std libs
+from numpydoc.docscrape import NumpyDocString
 import re
 import itertools as itt
 
@@ -13,6 +14,9 @@ import more_itertools as mit
 from .dicts import DefaultOrderedDict
 from .iter import nth_zip
 
+
+class NULL:
+    "Null singleton"
 
 def _echo(_):
     return _
@@ -68,14 +72,14 @@ def cosort(*lists, **kws):
     Examples
     --------
     Sorting multiple lists in sympathy:
-    >>> sortmore('32145', range(5))
+    >>> cosort('32145', range(5))
 
     Capture sorting indices:
     >>> l = list('CharacterS')
-    >>> sortmore(l, range(len(l)))
+    >>> cosort(l, range(len(l)))
     (['C', 'S', 'a', 'a', 'c', 'e', 'h', 'r', 'r', 't'],
      [0, 9, 2, 4, 5, 7, 1, 3, 8, 6])
-    >>> sortmore(l, range(len(l)), key=str.lower)
+    >>> cosort(l, range(len(l)), key=str.lower)
     (['a', 'a', 'C', 'c', 'e', 'h', 'r', 'r', 'S', 't'],
      [2, 4, 0, 5, 7, 1, 3, 8, 9, 6])
     """
@@ -125,55 +129,9 @@ def cosort(*lists, **kws):
     return tuple(map(list, zip(*res)))
 
 
-def sortmore(*lists, key=None, order=1):
-    return cosort(*lists, key=key, order=order)
+# def sortmore(*lists, key=None, order=1):
+#     return cosort(*lists, key=key, order=order)
 
-
-def sort_by_index(*its, index=None):
-    """Use index array to sort items in multiple sequences"""
-    if index is None:
-        return its
-    return tuple(list(map(it.__getitem__, ix))
-                 for it, ix in zip(its, itt.repeat(index)))
-
-
-def where(l, item, default=None):
-    """
-    A multi-indexer for lists. Return index locations of all occurances of `item`
-    in `l` as a list
-
-    Parameters
-    ----------
-    l : list
-        The list to search
-    item : object
-        Item to be searched for
-    default : object
-        The default return value in case `item` is not found, by default None
-
-    Returns
-    -------
-    list of int or None
-        The indices
-    """
-    return list(_where(l, item)) or default
-
-
-def _where(l, item):
-    i = 0
-    while i < len(l):
-        try:
-            i = l.index(item, i)
-            yield i
-        except ValueError:
-            # done
-            return
-        else:
-            i += 1  # start next search one on
-
-
-def flatten(l):
-    return list(mit.collapse(l))
 
 
 def lists(mapping):
@@ -181,94 +139,155 @@ def lists(mapping):
     return list(map(list, mapping))
 
 
+def sort_by_index(*its, indices=None):
+    """Use index array to sort items in multiple sequences"""
+    if indices is None:
+        return its
+    return tuple(list(map(it.__getitem__, ix))
+                 for it, ix in zip(its, itt.repeat(indices)))
+
+
+
+
+def where(l, item, start=0, default=NULL, test=None):
+    """
+    A multi-indexer for lists. Return index positions of all occurances of
+    `item` in a list `l`.  If a test function is given, return all indices at
+    which the test evaluates true.
+
+    Parameters
+    ----------
+    l : list
+        The list of items to be searched
+    item : object
+        item to be found
+    start : int, optional
+        optional starting index for the search, by default 0
+    default : object, optional
+        The default to return if `item` was not found in the input list, by
+        default None
+    test : callable, optional
+        Function used to identify the item. Calling sequence of this function is
+        `test(x, item)`, where `x` is an item from the input list. The function
+        should return boolean value to indicate whether the position of that
+        item is to be returned in the index list. The default is None, which
+        tests each item for equality with input `item`.
+
+    Examples
+    --------
+    >>> l = ['ab', 'Ba', 'cb', 'dD']
+    >>> where(l, 'a', str.__contains__)
+    [0, 1]
+    >>> where(l, 'a', indexer=str.startswith)
+    [0]
+
+
+    Returns
+    -------
+    list of int or `default`
+        The index positions where test evaluates true
+    """
+    if default is NULL:
+        default = []
+    return list(_where(l, item, start, test)) or default
+
+
+def _where(l, item, start=0, test=None):
+    i = 0
+    # l = l[start:]
+    while i < len(l) - start:
+        try:
+            yield index(l, item, i, test=test)
+        except ValueError:
+            # done
+            return
+        else:
+            i += 1  # start next search one on
+
+def index(l, item, start=0, default=None, test=None):
+    """
+    Find the index position of `item` in list `l`, or if a test function is
+    provided, the first index position for which the test evaluates as true. If
+    the item is not found, or no items test positive, return the provided
+    default value. 
+
+    {params}
+
+    Returns
+    -------
+    int
+        The index position where the first occurance of `item` is found, or
+        where `test` evaluates true
+    """
+    if test is None:
+        # test = item.__eq__
+        return l.index(item, start)
+
+    for i, x in enumerate(l[start:], start):
+        if test(x, item):
+            return i
+
+    return default
+
+
+index.__doc__ = index.__doc__.format(
+    params='\n\t'.join([''] + NumpyDocString(where.__doc__)._str_param_list('Parameters')))
+
+
+def flatten(l):
+    """
+    Flatten arbitrarily nested sequences
+
+    Parameters
+    ----------
+    l : list
+        [description]
+
+    Returns
+    -------
+    list
+        [description]
+    """
+    return list(mit.collapse(l))
+
+
+
 def split(l, idx):
     """Split a list into sublists at the given indices"""
     return list(map(l.__getitem__, itt.starmap(slice, mit.pairwise(idx))))
 
 
-def find(l, item, start=0, indexer=None):
-    """
-    List indexing with a bit of spice
-
-    Parameters
-    ----------
-    l : [type]
-        [description]
-    item : [type]
-        [description]
-    start : int, optional
-        [description], by default 0
-    indexer : [type], optional
-        [description], by default None
-
-    Returns
-    -------
-    [type]
-        [description]
-    """
-    if indexer is None:
-        return l.index(item, start)
-
-    for i, x in enumerate(l[start:]):
-        if indexer(x, item):
-            return i
-
-
-def findall(l, item, indexer=None):
-    """
-    Return the index positions of the items in the list.
-    Parameters
-    ----------
-    indexer:    function, optional
-        method by which the indexing is done.  Calling sequence is indexer(x, items), where x is an item
-        from the input list.  The function should return boolean value to indicate whether the position
-        of that item is to be returned in the index list.
-
-    Examples
-    --------
-    >>> l = ['ab', 'Ba', 'cb', 'dD']
-    >>> findall(l, 'a', str.__contains__)
-    [0, 1]
-    >>> findall(l, 'a', indexer=str.startswith)
-    [0]
-
-    """
-    indexer = indexer or (lambda x, i: (x == i))
-    return [i for (i, x) in enumerate(l) if indexer(x, item)]
-
-
-def item_split(l, items, withfirst=False, withlast=False, indexer=None):
+def item_split(l, item, test=None):
     """
     Split a list into sublists at the indices of the given item.
     """
-    idx = findall(l, items, indexer)
+    # idx = where(l, item, indexer)
 
-    if withfirst:
-        idx = [0] + idx
-    if withlast:
-        idx += [len(l) - 1]
+    # withfirst=False, withlast=False,
+    # if withfirst:
+    #     idx = [0] + idx
+    # if withlast:
+    #     idx += [len(l) - 1]
 
-    return split(l, idx)
+    return split(l, where(l, item, test))
 
 
 def re_find(l, pattern):
-    R = []
+    matches = []
     matcher = re.compile(pattern)
     for i, x in enumerate(l):
         m = matcher.match(x)
         if m:
-            R.append((i, m.group()))
-    return R
+            matches.append((i, m.group()))
+    return matches
 
 
-def find_missing_numbers(l):
+def missing_integers(l):
     """Find the gaps in a sequence of integers"""
-    all_numbers = set(range(min(l), max(l) + 1))
-    missing = all_numbers - set(l)
-    return sorted(missing)
+    return sorted(set(range(min(l), max(l) + 1)) - set(l))
 
 
-def tally(l):
+def unique(l):
     """Return dict of unique (item, indices) pairs for sequence."""
     t = DefaultOrderedDict(list)
     for i, item in enumerate(l):
@@ -276,25 +295,26 @@ def tally(l):
     return t
 
 
-def count_repeats(l):
+def tally(l):
     """Return dict of item, count pairs for sequence."""
     t = DefaultOrderedDict(int)
-    for i, item in enumerate(l):
+    for item in l:
         t[item] += 1
     return t
 
 
-def gen_duplicates(l):
+def iter_duplicates(l):
     """Yield tuples of item, indices pairs for duplicate values."""
-    tally_ = tally(l)
-    return ((key, locs) for key, locs in tally_.items() if len(locs) > 1)
+    for key, idx in unique(l).items():
+        if len(idx) > 1:
+            yield key, idx
 
 
 def duplicates(l):
     """Return tuples of item, indices pairs for duplicate values."""
-    return list(gen_duplicates(l))
+    return list(iter_duplicates(l))
 
 
 def where_duplicate(l):
     """Return lists of indices of duplicate entries"""
-    return nth_zip(1, *gen_duplicates(l))
+    return nth_zip(1, *iter_duplicates(l))
