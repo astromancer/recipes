d35f4c048f318bd375f5d8eca6ccb0b20f5b96db
astromancer <hannes@saao.ac.za>
2021-03-22 22:38:18 +0200

    move tests

--- tests/test_string.py
+++ tests/test_string.py
@@ -1,7 +1,5 @@
 from recipes.testing import Expect, Throws, mock, expected
 from recipes.string import Percentage, sub, title
-from recipes.string.brackets import match, remove, outermost, contained
-from recipes.functionals import negate
 import pytest
 
 
@@ -37,91 +35,6 @@ test_replace = Expect(title)(
 )
 
 
-# @pytest.mark.skip()
-@expected(
-    {mock('hello(world)', '()'):                 'world',
-     mock('unclosed((((((', '()'):               None,
-     mock('nested((([inside]))]]])))', '[]'):    'inside',
-     mock('<s>', '<>'):                          's',
-     mock('((())', '()'):                        None})
-def test_match_brackets(s, pair, result):
-    r, (i, j) = match(s, pair)
-    assert r == result
-    if r:
-        assert r == s[i+1:j]
-
-
-test_brackets_must_close = Expect(match)(
-    {mock('foo{bla', '{}'):                     ('bla', (3, None)),
-     mock('open(((((('):                        ('(((((', (4, None)),
-     mock('((())'):                             ('(())', (1, None)),
-     mock('(', must_close=True):                Throws(ValueError),
-     mock('also_open((((((', must_close=True):  Throws(ValueError),
-     mock('((())', must_close=True):            Throws(ValueError)}
-)
-# '(',
-#          'also_open((((((',
-#          '((())']
-
-
-# def test_brackets_must_close(s, b, e):
-#     sub, (_, j) = match_brackets(s, b, must_close=-1)
-#     assert sub == e
-#     assert j is None
-
-# @pytest.mark.skip()
-# @pytest.mark.parametrize(
-#     's, b, e',
-#     [('foo{bla', '{}', 'bla'),
-#      ('open((((((', '()', '((((('),
-#      ('((())', '()', '(())')])
-# def test_brackets_must_close(s, b, e):
-#     sub, (_, j) = match_brackets(s, b, must_close=-1)
-#     assert sub == e
-#     assert j is None
-
-# DEPRECATED
-# test_brackets_must_close_raises = \
-#     ExpectFailure(match_brackets, must_close=True)(
-#         ['(',
-#          'also_open((((((',
-#          '((())']
-#     )
-
-# test_brackets_must_close = Expect(match_brackets, must_close=True)(
-#     {'(':                   Throws(ValueError),
-#      'also_open((((((':     Throws(ValueError),
-#      '((())':               Throws(ValueError)}
-# )
-
-# @pytest.mark.parametrize('s', ['(', 'also_open((((((', '((())'])
-# def test_brackets_must_close_raises(s):
-#     with pytest.raises(ValueError):
-#         match_brackets(s, must_close=True)
-
-
-test_unbracket = Expect(remove)(
-    {mock.unbracket(''):                                '',
-     mock.unbracket('()'):                              '',
-     mock.unbracket('test()'):                          'test',
-     mock.unbracket('test(())'):                        'test',
-     mock.unbracket('test(()'):                         'test(()',
-     mock.unbracket('{[(foo)]}'):                       '{[foo]}',
-     mock.unbracket('{{{hello world!}}}', '{}'):        'hello world!',
-     mock.unbracket('{some,thing}{}', '{}', 
-                    condition=contained(',').within):
-                                                        'some,thing{}',
-     mock('{some,thing}{}', '{}', 
-          condition=negate(contained(',').within)):     '{some,thing}',                                                 
-     mock.unbracket('{{1}}', '{}', 0):                  '{{1}}',
-     mock.unbracket('{{1}}', '{}', 1):                  '{1}',
-     mock.unbracket('{{hi{} {x}}}', '{}',
-                    condition=outermost):                'hi{} {x}',
-     mock.unbracket('((hello) world)',
-                    condition=outermost):          '(hello) world'
-     }
-)
-
 # _unbracket('{{{hello world!}}}', '{}', condition=lambda x: '!' not in x)
 # Out[53]: '{{{hello world!}}}'
 
@@ -131,7 +44,7 @@ test_unbracket = Expect(remove)(
 
 # tests = ['root/{ch{{1,2},{4..6}},main{1,2},{1,2}test}*.png',
 #      ...:          'ch{{1,2},{4..6}},main{1,2},{1,2}test']
-#      ...: lists(map(bash.fucksplitter, tests))
+#      ...: lists(map(bash.splitter, tests))
 
 # [['root/{ch{{1,2},{4..6}},main{1,2},{1,2}test}*.png'],
 #  ['ch{{1,2},{4..6}}', 'main{1,2}', '{1,2}test']]
