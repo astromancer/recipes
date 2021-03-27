5802f15e2d813c4217f3f733542070d8eddd6b64
astromancer <hannes@saao.ac.za>
2021-03-27 14:48:09 +0200

    add `all` and `any` operators with optional test function 🎆

--- recipes/op.py
+++ recipes/op.py
@@ -5,12 +5,51 @@ support for default values
 
 import warnings
 from recipes.decor import raises
+import docsplice as doc
 from operator import attrgetter
+import builtins
+
+
+def any(itr, test=bool):
+    """
+    Return True if test(x) is True for any x in the iterable.
+    If the iterable is empty, return False.
+
+    This function performs exactly the same function as the builtin any, but 
+    additionally allows the item evaluation test to be user-specified.
+
+    Parameters
+    ----------
+    itr : iterable
+        Items to be evaluated.
+    test : callable, optional
+        Test function for items in the iterable. Must return a bool. Default 
+        is the builtin bool function.
+
+    Returns
+    -------
+    bool
+
+    Examples
+    --------
+    >>> any(('U', 'X'), str.isupper)
+    True
+    """
+    return builtins.any(map(test, itr))
+
+
+@doc.splice(any, replace={'any': 'all', 'False': 'True'})
+def all(itr, test=bool):
+    return builtins.all(map(test, itr))
+
 
 class itemgetter:
-    def __init__(self, *keys, default=KeyError, **defaults):
+    """
+    Itemgetter
+    """
 
-        self.keys = tuple(map(str, keys))
+    def __init__(self, *keys, default=KeyError, **defaults):
+        self.keys = keys
         self.defaults = defaults
         typo = set(self.defaults.keys()) - set(self.keys)
         if typo:
