e58c3eef2c4229baffd0919a5d177d2cb62c8de5
astromancer <hannes@saao.ac.za>
2021-05-04 20:24:23 +0200

    docstrings + cleanup

--- recipes/string/brackets.py
+++ recipes/string/brackets.py
@@ -31,21 +31,14 @@ __all__ = ['Brackets', 'braces', 'square', 'round', 'chevrons']
 # angle brackets
 
 
-def always_true(_):
-    return True
+class contained:  # pylint: disable=invalid-name
+    """
+    Helper class for condition testing substring containment in strings
 
-
-class Yielder:
-    """Helper class for iterating and optionally yielding indices"""
-
-    def __init__(self, start=0):
-        self.start = int(start)
-
-    def yields(self, inside, i, j):
-        return inside
-
-    def with_index(self, inside, i, j):
-        return inside, (self.start + i, self.start + j)
+    Example
+    >>> [*map(contained('*').within, ['', '**', '..'])]
+    [False, True, False]
+    """
 
     def __init__(self, item):
         self.item = item
@@ -54,7 +47,7 @@ class Yielder:
         return self.item in container
 
 
-def outermost(string, brackets, indices, nr):
+def outermost(string, brackets, indices, _ignored):
     i, j = indices
     return remove_affix(string, *brackets) == string[i+1:j]
 
@@ -118,30 +111,34 @@ class Brackets:
 
         Parameters
         ----------
-        string : str 
-            The string to parse. 
-        return_index : bool 
-            Whether to return the indices where the brackets where found. 
-        must_close : int 
-            Controls behaviour on unmatched bracket pairs. If 1 or True a
-            ValueError will be raised, if 0 or False `None` will be returned
-            even if there is an opening bracket.  If -1, partial matches are
-            allowed and the partial string beginning one character after the
-            opening bracket will be returned. In this case, if `return_index` is
-            True, the index of the closing brace position will take the value
-            None.
+        string : str
+            The string to parse.
+        return_index : bool
+            Whether to return the indices where the brackets where found.
+        must_close : int
+            Controls behaviour on unmatched bracket pairs.
+            If 1 or True:
+                ValueError will be raised
+            if 0 or False:
+                All unclosed brackets are ignored. ie. `None` will be returned
+                even if there is an opening bracket.
+            If -1:
+                Partial matches are allowed and the partial string beginning one
+                character after the opening bracket will be returned. In this
+                case, if `return_index` is True, the index of the closing brace
+                position will take the value None.
 
         Examples
         --------
         >>> s = 'def sample(args=(), **kws):'
         >>> r, (i, j) = Brackets('()').match(s)
-        ('args=(), **kws' , (10, 25)) 
+        ('args=(), **kws' , (10, 25))
         >>> r == s[i+1:j]
         True
 
         Returns
         -------
-        match : str or None 
+        match : str or None
             The enclosed str index: tuple or None (start, end) indices of the
             actual brackets that were matched
 
@@ -198,7 +195,7 @@ class Brackets:
         string : str
             String potentially containing pairs of (nested) brackets.
         return_index : bool, optional
-            Whether to yield the indices of opening- closing bracket pairs, 
+            Whether to yield the indices of opening- closing bracket pairs,
             by default Tru
 
         Yields
@@ -208,8 +205,6 @@ class Brackets:
         indices : (int, int)
             Index positions of opening and closing bracket pairs.
         """
-        yielder = Yielder()
-        yields = yielder.with_index if return_index else yielder.yields
 
         # get condition test call signature
         test = get_test(condition)
@@ -279,7 +274,7 @@ class Brackets:
 
         Examples
         --------
-        >>> 
+        >>>
         """
         inner = self.match(string, False)
         return remove_affix(string, *self.brackets) == inner
@@ -357,14 +352,13 @@ def get_test(condition):
     if npar == 4:
         return condition
 
-    elif npar == 1:
+    if npar == 1:
         def test(string, brackets, indices, nr):
             i, j = indices
             return condition(string[i+1:j])
-    else:
-        raise ValueError('Condition test function has incorrect signature')
+        return test
 
-    return test
+    raise ValueError('Condition test function has incorrect signature')
 
 
 # predifined bracket types
@@ -397,114 +391,3 @@ def remove(string, brackets, depth=math.inf, condition=always_true):
 def strip(string, brackets):
     return Brackets(brackets).strip(string)
 
-
-# def _iter(string, brackets='()', return_index=True, must_close=False,
-#                   condition=always_true, depth=math.inf, level=0):
-#     """
-#     Iterate through consecutive (non-nested) closed bracket pairs.
-
-#     Parameters
-#     ----------
-#     string : [type]
-#         [description]
-#     brackets : str, optional
-#         [description], by default '()'
-#     return_index : bool, optional
-#         [description], by default True
-
-#     Yields
-#     -------
-#     [type]
-#         [description]
-#     """
-#     if return_index:
-#         def yields(inside, i, j):
-#             return inside, (start + i, start + j)
-#     else:
-#         def yields(inside, i, j):
-#             return inside
-
-#     start = 0
-#     while True:
-#         inside, (i, j) = match_brackets(string, brackets, True, must_close)
-#         if inside is None:
-#             break
-
-#         if condition(inside):
-#           # THIS WILL UNPACK FROM THE INNERMOST OUTWARDS
-#            yield from _iter(inside, )
-
-#         start = j + 1
-#         string = string[start:]
-
-
-# def _iter_deep(string, brackets, return_index=True, must_close=False,
-#                    condition=always_true, depth=math.inf, level=0):
-#     # return if too deep
-#     if level > depth:
-#         return string
-
-#     # inside, (i, j) = match_brackets(string, brackets, must_close=False)
-#     # if inside is None or not condition(inside):
-#     #     return string
-
-#     tail = string[i:]
-#     ran = False
-#     for inside, (i, j) in iter_brackets(tail, brackets, return_index, must_close,
-#                                         condition, level, level):
-#         # THIS WILL UNPACK FROM THE INNERMOST OUTWARDS
-#         yield from iter_brackets(inside, brackets, return_index, must_close,
-#                                   condition, depth, level+1)
-#         ran = True
-
-#     if not ran:
-#         yield inside
-
-
-# #
-#     itr = iter_brackets(string, brackets, condition=test)
-#     try:
-#         inside, (i, j) = next(itr)
-#     except StopIteration:
-#         return string
-#     else:
-#         out = string[:i]
-#         tail = string[j + 1:]
-#         nr = -1
-#         for nr, (inside, _) in enumerate(itr):
-#             out += _unbracket(inside, brackets, test, depth, level + 1, nr)
-
-#         # did the loop run?
-#         if nr > -1: # yes
-#             return out + tail
-
-#         # no? recurse on whatever is inside the brackets
-#         return out + _unbracket(inside, brackets, test, depth, level + 1, nr) + tail
-
-
-# # # alternatively
-#     inside, (i, j) = match_brackets(string, brackets, must_close=False)
-#     if inside is None or not test(inside, (i, j), nr):
-#         return string
-
-#     # testing on sequence number for every element is probably less efficient
-#     # than slicing the iterator below. Can think of a good way of
-#     # implementing that? is there even use cases?
-#     # if condition is outermost:
-
-#     out = string[:i]
-#     tail = string[i:]
-#     ran = False
-#     itr = iter_brackets(tail, brackets, must_close=True, condition=test)
-#     for nr, (inside, (i, j)) in enumerate(itr):
-#         out += _unbracket(inside, brackets, test, depth, level+1, nr)
-#         ran = True
-
-#     if not ran:
-#         out += inside
-
-#     return out + tail[j + 1:]
-
-# def to_fstring(string):
-#"'%s(xy=(%g, %g), ' % (self.__class__.__name__, *self.center)"
-# ---> 'self.__class__.__name__ + '(xy=(%g, %g), ' % self.center'
