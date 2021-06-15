22bc1a0cdf7722655d77fcf60581ea39f82c8741
astromancer <hannes@saao.ac.za>
2021-06-15 16:44:22 +0200

    re-add mistakenly removed module

--- tests/decor/test_base.py
+++ tests/decor/test_base.py
@@ -1,7 +1,66 @@
 from recipes.decor.base import Decorator
 
+from pytest_cases import parametrize, fixture
 
-class Foo():
+FUNC_TEMPLATE = """
+@Decorator{argspec}
+def {name}():
+    return True
+"""
+CLASS_TEMPLATE = """
+class {name}:
+    {pre}
+    @Decorator{argspec}
+    {post}
+    def method(self):
+        return True
+"""
+DEFAULTS = {'pre': '', 'post': ''}
+
+
+# @fixture(scope='session',
+#          params=['', '()', '(ignored)', '(0, kwo=0, **{"kw":0})'])
+# def argspec(request):
+#     return request.param
+
+
+# def _make(template, **kws):
+#     code = template.format(**{**DEFAULTS, **kws})
+#     locals_ = {}
+#     exec(code, None, locals_)
+#     obj = locals_[kws['name']]
+#     obj.__source__ = code
+#     return obj
+
+
+# def _generic_test(template, objname, argspec, **kws):
+#     assert _make(template, name=objname, argspec=argspec, **kws)()
+
+
+# class TestDecorator:
+
+#     def test_func(self, argspec):
+#         _generic_test(FUNC_TEMPLATE, 'func', argspec)
+
+#     def test_class(self, argspec):
+#         _generic_test(CLASS_TEMPLATE, 'Class', argspec)
+
+#     @parametrize(pre=['@classmethod', ''],
+#                  post = ['', '@classmethod'])
+#     def test_classmethod(self, argspec, pre, post):
+#         _generic_test(CLASS_TEMPLATE, 'Class', argspec, pre=pre, post=post)
+        
+        
+
+        
+
+@count_calls
+def foo(a):
+    ...
+    
+# [*map(foo, '12345')]
+
+class Foo:
     @Decorator
     def method1(self):
         print('method1')
