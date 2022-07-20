9fbbbcdb5950b64bce42cabb57767386080ab225
astromancer <hannes@saao.ac.za>
2022-07-20 17:12:09 +0200

    node dynamically spaced rendering  refactor

--- src/recipes/tree.py
+++ src/recipes/tree.py
@@ -3,17 +3,137 @@
 # std
 import re
 import math
+import numbers
 import itertools as itt
+from typing import MutableMapping
 
 # third-party
 import anytree
+from anytree import AbstractStyle, ContRoundStyle
+from anytree.render import RenderTree, Row, _is_last
+from loguru import logger
+
+# local
+from recipes.oo.temp import temporarily
 
 # relative
 from . import op
 from .string import remove_prefix, strings
 
 
-class Node(anytree.Node):
+# ---------------------------------------------------------------------------- #
+# for style in AbstractStyle.__subclasses__():
+#     style().end[0]
+
+RGX_TREE_PARSER = re.compile('([│├└ ])[─ ]{2} ')
+
+
+def _reindent(string, height):
+    # regex HACK
+    first, *lines = string.splitlines()
+    yield first + '\n'
+    indents = [len(first) - 1] + [0] * height
+    for row in lines:
+        matches = [*RGX_TREE_PARSER.finditer(row)]
+        if depth := len(matches):
+            end = matches[-1].end()
+            indents[depth] = len(row) - end - 1
+            for m, i in zip(matches, indents):
+                yield ' ' * i + m[1]
+            yield row[end:]  # + '┐'
+        else:
+            yield row
+        yield '\n'
+
+# ---------------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------------- #
+
+
+class DynamicIndentRender(RenderTree):
+
+    def __init__(self, node, style=ContRoundStyle(), childiter=list, maxlevel=None):
+        super().__init__(node, style, childiter, maxlevel)
+
+        self.attr = 'name'
+        self.widths = [0] * (node.height + 1)
+        # Adapt the style
+        # Use first character of vertical/branch/end strings eg "│", "├", "└"
+        style = self.style
+        self.style = AbstractStyle(*next(zip(style.vertical, style.cont, style.end)))
+
+    def __iter__(self):
+        return self.__next(self.node, tuple())
+
+    def __next(self, node, continues, level=0):
+        name = str(getattr(node, self.attr))
+        self.widths[(level - 1):] = [len(name), *([0] * (node.height + 1))]
+        # print(f'next {node=:}, {level=:}, {self.indents=:}, {continues=:}')
+
+        yield self.__item(node, continues, self.style, level)
+
+        children = node.children
+        level += 1
+        if children and (self.maxlevel is None or level < self.maxlevel):
+            children = self.childiter(children)
+            for child, is_last in _is_last(children):
+                yield from self.__next(child, continues + (not is_last, ), level=level)
+
+    def __item(self, node, continues, style, level):
+
+        if not continues:
+            return Row(u'', u'', node)
+        selection = (style.empty, style.vertical)
+        items = [f'{selection[c]: <{w}}' for w, c in zip(self.widths, continues)]
+        branch = f'{style.cont if continues[-1] else style.end: <{self.widths[level]}}'
+        indent = ''.join(items[:-1])
+        pre = indent + branch
+        fill = ''.join((indent, items[-1]))
+        # print(f'item {pre=:}, {fill=:}, {node=:}')
+        return Row(pre, fill, node)
+
+    def by_attr(self, attrname='name'):
+        with temporarily(self, attr=attrname):
+            return super().by_attr(attrname)
+
+class PrintNode(anytree.Node):
+
+    # rendering option
+    renderer = DynamicIndentRender
+
+    def render(self):
+        """
+        A re-spaced rendering of the tree where spacing between levels are set
+        dynamically based on the length of the string representation of the
+        parents.
+
+        For example:
+            └20
+              ├13061
+              │    ├6.003
+              │    │    ├0
+              │    │    └1
+              │    ├7.003
+              │    │    ├0
+              │    │    └1
+              │    └8.003
+              │         ├0
+              │         └1
+              └2130615.003
+                         ├0
+                         ├1
+                         └2
+        """
+        return str(self.renderer(self))
+
+    def pprint(self):
+        print(self.render())
+
+# ---------------------------------------------------------------------------- #
+
+
+class Node(PrintNode):  # StringNode
     """
     An `anytree.Node` that builds trees based on a list of input strings.
     Can be used to represent file system trees etc.
@@ -23,8 +143,9 @@ class Node(anytree.Node):
     """This function decides the names of the nodes. It will be called on each
     string in the input list, returning the name of the of that node parent."""
 
-    # rendering option
-    use_dynamic_spacing = True
+    @property
+    def sibling_leaves(self):
+        return tuple(child for child in self.siblings if child.is_leaf)
 
     @classmethod
     def from_list(cls, items, collapse=True):
@@ -36,7 +157,7 @@ class Node(anytree.Node):
         items : list
             _description_
 
-        Examples
+        Examples 
         --------
         >>> fonts = Node.from_list(['Domitian-Bold.otf',
         ...                         'Domitian-BoldItalic.otf',
@@ -152,25 +273,25 @@ class Node(anytree.Node):
         │        ├Italic.otf
         │        └Roman.otf
         └EBGaramond-
-                ├Bold
-                │   ├.otf
-                │   └Italic.otf
-                ├ExtraBold
-                │        ├.otf
-                │        └Italic.otf
-                ├I
-                │├nitials.otf
-                │└talic.otf
-                ├Medium
-                │     ├.otf
-                │     └Italic.otf
-                ├Regular.otf
-                └SemiBold
-                        ├.otf
-                        └Italic.otf
+                   ├Bold
+                   │   ├.otf
+                   │   └Italic.otf
+                   ├ExtraBold
+                   │        ├.otf
+                   │        └Italic.otf
+                   ├I
+                   │├nitials.otf
+                   │└talic.otf
+                   ├Medium
+                   │     ├.otf
+                   │     └Italic.otf
+                   ├Regular.otf
+                   └SemiBold
+                           ├.otf
+                           └Italic.otf
 
         >>> fonts.collapse(max_depth=2)
-        ... fonts.pprint()
+        >>> fonts.pprint()
 
         ├Domitian-
         │        ├Italic.otf
@@ -178,17 +299,22 @@ class Node(anytree.Node):
         │        ├Bold.otf
         │        └BoldItalic.otf
         └EBGaramond-
-                ├Regular.otf
-                ├Bold.otf
-                ├BoldItalic.otf
-                ├ExtraBold.otf
-                ├ExtraBoldItalic.otf
-                ├Initials.otf
-                ├Italic.otf
-                ├Medium.otf
-                ├MediumItalic.otf
-                ├SemiBold.otf
-                └SemiBoldItalic.otf
+                   ├Regular.otf
+                   ├Bold.otf
+                   ├BoldItalic.otf
+                   ├ExtraBold.otf
+                   ├ExtraBoldItalic.otf
+                   ├Initials.otf
+                   ├Italic.otf
+                   ├Medium.otf
+                   ├MediumItalic.otf
+                   ├SemiBold.otf
+                   └SemiBoldItalic.otf
+
+        Note that, in this example, the root node's `name` attribute is
+        just an empty string '', which formats as a newline in string
+        representation above.
+
 
         Returns
         -------
@@ -278,54 +404,6 @@ class Node(anytree.Node):
             changed |= child.collapse_unary()
 
         return changed
-
-    def render(self):
-        """
-        A re-spaced rendering of the tree where spacing between levels are set
-        dynamically based on the length of the string representation of the
-        parents.
-
-        For example:
-            └20
-              ├13061
-              │    ├6.003
-              │    │    ├0
-              │    │    └1
-              │    ├7.003
-              │    │    ├0
-              │    │    └1
-              │    └8.003
-              │         ├0
-              │         └1
-              └2130615.003
-                         ├0
-                         ├1
-                         └2
-        """
-        s = str(anytree.RenderTree(self))
-
-        if not self.use_dynamic_spacing:
-            return s
-
-        pre = re.compile('([│├└ ])[─ ]{2} ')
-        first, *lines = s.splitlines()
-        new = first + '\n'
-        indents = [len(first) - 1] + [0] * self.height
-        for row in lines:
-            matches = [*pre.finditer(row)]
-            if depth := len(matches):
-                end = matches[-1].end()
-                indents[depth] = len(row) - end - 1
-                for m, i in zip(matches, indents):
-                    new += ' ' * i + m[1]
-                new += row[end:]  # + '┐'
-            else:
-                new += row
-            new += '\n'
-        return new
-
-    def pprint(self):
-        print(self.render())
 
 
 class FileSystemNode(Node):
