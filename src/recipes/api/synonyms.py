fdd5669976733be657ada9986a8b11dfa93327bd
astromancer <hannes@saao.ac.za>
2022-08-29 09:44:47 +0200

    move towards pure regex translation

--- src/recipes/api/synonyms.py
+++ src/recipes/api/synonyms.py
@@ -1,5 +1,5 @@
 """
-Parameter and Keyword name translation for understanding inexact human inputself.
+Parameter and Keyword name translation for understanding inexact human input.
 
 Say you have an API function:
 
@@ -9,7 +9,7 @@ Say you have an API function:
 Your users might attempt the following:
 >>> stamp(centre=True, colour='green', labelled='order 66')
 unwittingly having used a variant spelling of the parameter names that is
-natural to theor local. This will of course lead to an error. Unless...
+natural to their local. This will of course lead to an error. Unless...
 
 We can teach out function these alternatives like so:
 >>> @api.synonymns(dict(
@@ -35,6 +35,7 @@ regexes above at the cost of a small overhead for each misspelled parameter.
 import re
 import inspect
 import itertools as itt
+from typing import Type
 
 # third-party
 from loguru import logger
@@ -43,6 +44,7 @@ from loguru import logger
 from ..dicts import groupby
 from ..decorators import Decorator
 from ..string.brackets import BracketParser
+from ..functionals import Emit
 
 
 def _ordered_group(word):
@@ -57,49 +59,75 @@ OPTION_REGEX_BUILDERS = {'[]': _ordered_group,
                          '()': _unordered_group}
 
 
-class KeywordTranslator:
+class RegexTranslate:
     """
     Class to assist many-to-one keyword mappings via regex pattern matching.
     """
-    parser = BracketParser('[]', '()')
 
     def __init__(self, pattern, answer=''):  # ensure_order
-        """
-        Implements a basic pattern matching syntax to match incomplete words.
-        Optional characters that need to appear in order should be enclosed in
-        square brackets eg: "n[umbe]r". This pattern will match "number" as well
-        as "numr" and "nr", but not "nuber". Optional characters that can have
-        any order should be enclosed in round brackets eg: "frivol(ou)s".
 
-        Note that input patterns are translated to regex, so eg:
-            "n[umbe]r" --> "n(u|um|umb|umbe)r"
-            "n(umber)" --> "n[umber]{0,5}"
-        Any other regex syntax in the pattern will pass through unaltered. You 
-        can use this to your adbvantage. For example to match a single optional
-        optional character, either enclose it in round brackets, or append a "?"
-        eg: "col(_)head" and "col_?head" both work to match the optional
-        underscore.
+        # assert mode in {'simple', 'regex', None}, f'Invalid mode string: {mode!r}'
 
-        Parameters
-        ----------
-        pattern : str
-            Pattern for matching parameter / keyword names.
-        answer: str
-            The desired translation target.
-
-        Example
-        -------
-        >>> resolve = KeywordTranslator('n[umbe]r[_rows]', 'row_nrs')
-        >>> resolve('number_rows')
-        'row_nrs' 
-        >>> resolve('nr_rows')
-        'row_nrs'
-        >>> resolve('nrs')
-        'row_nrs'
-        """
-
-        self.pattern = pattern
         self.answer = str(answer)
+
+        if isinstance(pattern, str):
+            self.regex = re.compile(pattern)
+
+        elif isinstance(pattern, re.Pattern):
+            self.regex = pattern
+
+        else:
+            raise TypeError('Invalid pattern type: {}', type(pattern))
+
+    def __call__(self, s):
+        if self.regex.fullmatch(s):
+            return self.answer
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.regex.pattern} --> {self.answer})'
+
+
+class KeywordTranslate(RegexTranslate):
+    """
+    Implements a basic pattern matching syntax to match incomplete words.
+    Optional characters that need to appear in order should be enclosed in
+    square brackets eg: "n[umbe]r". This pattern will match "number" as well
+    as "numr" and "nr", but not "nuber". Optional characters that can have
+    any order should be enclosed in round brackets eg: "frivol(ou)s".
+
+    Note that input patterns are translated to regex, so eg:
+        "n[umber]" --> "n(u|um|umb|umbe|umber)" "n(u(m(b(e(r?)?)?)?)?)?"
+        "n(umbe)r" --> "n(umbe)?r"
+    Any other regex syntax in the pattern will pass through unaltered. You 
+    can use this to your advantage. For example to match a single optional
+    optional character, either enclose it in round brackets, or append a "?"
+    eg: "col(_)head" and "col_?head" both work to match the optional
+    underscore.
+
+    Parameters
+    ----------
+    pattern : str
+        Pattern for matching parameter / keyword names.
+    answer: str
+        The desired translation target.
+
+    Example
+    -------
+    >>> resolve = KeywordTranslate('n[umbe]r[_rows]', 'row_nrs')
+    >>> resolve('number_rows')
+    'row_nrs'
+    >>> resolve('nr_rows')
+    'row_nrs'
+    >>> resolve('nrs')
+    'row_nrs'
+    """
+
+    parser = BracketParser('[]', '()')
+
+    def __init__(self, pattern, answer=''):
+        super().__init__(self._build_regex(pattern), answer)
+
+    def _build_regex(self, pattern):
         sub = pattern
         regex = ''
         while 1:
@@ -116,18 +144,12 @@ class KeywordTranslator:
 
             regex += f'{sub[:i0]}{s}?'
             # self.answer += sub[:i0]
+            # FIXME: nesting?
             sub = sub[i1 + 1:]
 
             # print(sub, regex)
             # i += 1
-        self.regex = re.compile(regex)
-
-    def __call__(self, s):
-        if self.regex.fullmatch(s):
-            return self.answer
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}({self.pattern} --> {self.answer})'
+        return regex
 
 
 POS, PKW, VAR, KWO, VKW = inspect._ParameterKind
@@ -141,31 +163,49 @@ class Synonyms(Decorator):
 
     # TODO: detect ambiguous mappings
 
-    def __init__(self, mappings=(), /, _emit=-1, **kws):
-        self.emit = logger.debug    # Emit(_emit)
+    def __init__(self, mappings=(), /, mode='simple', emit='warn'):
+        self.emit = Emit(emit, TypeError)
         self.resolvers = []
-        self.update(mappings, **kws)
+        self.update(mappings, mode)
         self.func = None
         self._param_names = ()
+
+    def __call__(self, func):
+        self.func = func
+        self.signature = inspect.signature(func)
+        self._param_names = tuple(self.signature.parameters.keys())
+        return super().__call__(func)
+
+    def __wrapper__(self, func, *args, **kws):
+        args, kws = self.resolve(args, kws)  
+        return func(*args, **kws)
+
+        # except TypeError as err: # FIXME: only works if func has no var kws
+        #     if ((e := str(err)).startswith(func.__name__) and
+        #             ('unexpected keyword argument' in e)):
+        #         #
+        #         logger.debug('Caught {}\n. Attempting keyword translation.', err)
+        #         args, kws = self.resolve(args, kws)
+        #         logger.debug('Re-trying function call {}(...) with synonymous '
+        #                      'keywords.', func.__name__)
+        #         return func(*args, **kws)
+
+        #     raise
 
     def __repr__(self):
         return repr(self.resolvers)
 
-    def __wrapper__(self, func, *args, **kws):
-        self.func = func
-        self.signature = inspect.signature(func)
-        self._param_names = tuple(self.signature.parameters.keys())
-
-        args, kws = self.resolve(args, kws)
-        return func(*args, **kws)
-
-    def update(self, mappings=(), **kws):
+    def update(self, mappings=(), /, _mode='simple'):
         """
         Update the translation map.
         """
-        for directive, target in dict(mappings, **kws).items():
+
+        translator = {'simple': KeywordTranslate,
+                      'regex': RegexTranslate}[_mode]
+        
+        for directive, target in dict(mappings).items():
             # if isinstance(k, str)
-            self.resolvers.append(KeywordTranslator(directive, target))
+            self.resolvers.append(translator(directive, target))
 
     def resolve(self, args, kws):  # namespace=None,
         """
