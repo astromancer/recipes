1dba8d1889c78eabf24ec0535254205026af87d2
astromancer <hannes@saao.ac.za>
2023-11-28 23:58:18 +0200

    TST: restructure tests

--- tests/functionals/test_partial.py
+++ tests/functionals/test_partial.py
@@ -1,28 +1,9 @@
 
-from recipes.decorators import update_defaults
-from recipes.functionals.partial import partial, placeholder
-
-
-@update_defaults(dict(nwindow=1,
-                      noverlap=2,
-                      kmax=3))
-def flag_outliers(bjd, flux,
-                  nwindow=0,
-                  noverlap=0,
-                  kmax=0):
-    pass
-
-
-def test_update_defaults():
-    assert flag_outliers.__defaults__ == (1, 2, 3)
+from recipes.functionals.partial import partial, placeholder as o
 
 
 def func(a, b, c, q=0, **kws):
     return (a, b, c, q)
-
-
-# special placeholder
-O = placeholder
 
 
 def test_partial():
@@ -30,13 +11,13 @@ def test_partial():
     kws = {'hi': 0}
 
     # New partial function with one free parameter (originally position)
-    func_partial_at_2_only = partial(func)('a', 'b', O, q=1, **kws)
+    partial_at_2 = partial(func)('a', 'b', o, q=1, **kws)
     # later
-    result = func_partial_at_2_only(2)
+    result = partial_at_2(2)
     assert result == ('a', 'b', 2, 1)
     # >>> func('a', 'b', 2)
 
-    func_partial_at_1_and_2 = partial(func)('a', O, O, q=1, **kws)
+    func_partial_at_1_and_2 = partial(func)('a', o, o, q=1, **kws)
     #  later
     result = func_partial_at_1_and_2(1, 2)
     assert result == ('a', 1, 2, 1)
@@ -45,20 +26,20 @@ def test_partial():
     partial_none = partial(func)(1, 2, 3, 4)
     result = partial_none(hi=1)
     assert result == (1, 2, 3, 4)
-    
-    
+
+
 def test_partial_indexed():
 
     kws = {'hi': 0}
 
     # New partial function with one free parameter (originally position)
-    func_partial_at_2_only = partial(func)('a', 'b', O[0], q=1, **kws)
+    func_partial_at_2_only = partial(func)('a', 'b', o[0], q=1, **kws)
     # later
     result = func_partial_at_2_only('12')
     assert result == ('a', 'b', '1', 1)
     # >>> func('a', 'b', '12'[0])
 
-    func_partial_at_1_and_2 = partial(func)('a', O[0], O[1], q=1, **kws)
+    func_partial_at_1_and_2 = partial(func)('a', o[0], o[1], q=1, **kws)
     #  later
     result = func_partial_at_1_and_2('01', '01')
     assert result == ('a', '0', '1', 1)
@@ -68,3 +49,23 @@ def test_partial_indexed():
     result = partial_none(hi=1)
     assert result == (1, 2, 3, 4)
 
+
+# ---------------------------------------------------------------------------- #
+class _TestObject:
+    x = 1
+    y = 'abc'
+
+
+def test_partial_lookup():
+    kws = {'hi': 0}
+    partial_lookup = partial(func)('a', 'b', o.x, q=1, **kws)
+    result = partial_lookup(_TestObject)
+    assert result == ('a', 'b', 1, 1)
+    # >>> func('a', 'b', _TestObject.x, 1)
+
+
+def test_partial_lookup_slice():
+    kws = {'hi': 0}
+    partial_lookup_slice = partial(func)('a', 'b',  o.y[2], q=1, **kws)
+    result = partial_lookup_slice('a', _TestCase, '1')
+    assert result == ('a', 'b', 'c', 1)
