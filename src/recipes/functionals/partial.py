aa4e971ea01773f84339ae0ac1a5234762bbd260
astromancer <hannes@saao.ac.za>
2023-11-28 22:05:41 +0200

    DEV: `Partial` decorator now supports placeholder attribute lookup 💡

--- src/recipes/functionals/partial.py
+++ src/recipes/functionals/partial.py
@@ -25,27 +25,52 @@ from ..decorators import Decorator
 # from ..oo.slots import SlotHelper # Circilar!
 
 
-# TODO map(partial(sub)(vector(subs.values()), subs))
-# Can probably help do this with type annotations
+def _resolve_object(obj, placeholder):
+
+    while placeholder is not PlaceHolder:
+        if isinstance(placeholder, _FutureSlice):
+            obj = obj[placeholder.key]
+        elif isinstance(placeholder, _FutureLookup):
+            obj = getattr(obj, placeholder.key)
+        placeholder = placeholder.parent
+
+    return obj
 
 
 class PlaceHolder:
-    # Singleton representing any omitted parmeter
+    # Singleton representing any omitted parameter
     def __new__(cls):
         return PlaceHolder
 
     def __class_getitem__(cls, key):
-        return _IndexedPlaceHolder(key)
+        return _FutureSlice(key, cls)
+
+    def __getattr__(cls, attr):
+        return _FutureLookup(attr, cls)
 
 
-class _IndexedPlaceHolder(PlaceHolder):
-    __slots__ = ('key', ) 
-    
-    def __init__(self, key):
+class _FutureSlice(PlaceHolder):
+    # object representing an indexed placeholder
+
+    __slots__ = ('key', )
+
+    def __init__(self, key, parent=None):
         self.key = key
-    
+        self.parent = parent
+
     def __new__(cls, *args, **kws):
-        return object.__new__(cls) # not a singleton!
+        return object.__new__(cls)  # not a singleton!
+
+    def __getattr__(self, attr):
+        return _FutureLookup(attr, self)
+
+    def __getitem__(self, key):
+        return _FutureSlice(key, self)
+
+
+class _FutureLookup(_FutureSlice):
+    # object representing attribute lookup on a placeholder
+    pass
 
 
 class PartialAt(Decorator):
@@ -53,11 +78,9 @@ class PartialAt(Decorator):
     def __init__(self, args, kws):
         self.args = list(args)
         self._positions = tuple(where(args, PlaceHolder))
-        self._positions_indexed = tuple(where(args, isinstance, _IndexedPlaceHolder))
 
         self.kws = kws = dict(kws)
         self._keywords = tuple(where(kws, PlaceHolder))
-        self._keywords_indexed = tuple(where(kws, isinstance, _IndexedPlaceHolder))
 
     def __call__(self, func):
         return super().__call__(func, emulate=False, kwsyntax=True)
@@ -67,7 +90,7 @@ class PartialAt(Decorator):
 
     @property  # cache?
     def nfree(self):
-        return len(self._positions) + len(self._positions_indexed)
+        return len(self._positions)
 
     def _get_args(self, args=()):
         # fill placeholders in `self.args` with dynamic values from `args` here
@@ -79,42 +102,37 @@ class PartialAt(Decorator):
 
         # shallow copy
         _args = self.args[:]
-        
+
         # fill
         for i, a in zip(self._positions, args):
-            _args[i] = a
-
-        for i, a in zip(self._positions_indexed, args):
-            _args[i] = a[_args[i].key]
+            _args[i] = _resolve_object(a, self.args[i])
 
         return tuple(_args)
 
     def _get_kws(self, kws):
-        # fill placeholders in `self.kws` with dynamic values from `kws`
-        # here to get final key-word parameters for the function call
-        if undespecified := set(self._keywords + self._keywords_indexed) - set(kws.keys()):
-            raise ValueError(f'Required Keyword arguments: {undespecified}.')
+        # fill placeholders in `self.kws` with dynamic values from `kws` here to
+        # get final keyword parameters for the function call
 
-        # shallow copy 
+        if undespecified := set(self._keywords) - set(kws.keys()):
+            raise ValueError(f'Required keyword arguments {undespecified} not found.')
+
+        # shallow copy
         out = self.kws.copy()
-        
+
         # fill
         for key in self._keywords:
-            out[key] = kws[key]
-            
-        for key in self._keywords_indexed:
-            out[key] = kws[key][out[key].key]
+            out[key] = _resolve_object(kws[key], self.kws[key])
 
         return out
 
 
 class Partial(Decorator):
-    
+
     Task = PartialAt
 
     def __call__(self, func, emulate=False, kwsyntax=False):
         return super().__call__(func, emulate, kwsyntax)
-    
+
     def __wrapper__(self, func, *args, **kws):
         return self.Task(args, kws)(func)
 
