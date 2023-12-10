f44869d9a2f57c2260c00fb2f4e0107d08f46d1d
astromancer <hannes@saao.ac.za>
2023-12-10 13:14:30 +0200

    FIX: add missing import

--- src/recipes/string/__init__.py
+++ src/recipes/string/__init__.py
@@ -7,5 +7,6 @@ from .casing import *
 from .affixes import *
 from .justify import *
 from .plurals import *
+from .stacking import *
 from .percentage import Percentage
 # from .brackets import BracketParser # circular import!
