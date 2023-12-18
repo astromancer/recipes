be59be901046938638984a0ea131da660ba0422e
astromancer <hannes@saao.ac.za>
2023-12-18 23:05:36 +0200

    DEV: `Decorator` now allows custom wrapper class

--- src/recipes/decorators/base.py
+++ src/recipes/decorators/base.py
@@ -8,29 +8,38 @@ from decorator import decorate
 
 # ---------------------------------------------------------------------------- #
 class Wrapper:
-    """Transparent wrapper for callables."""
+    """
+    Transparent wrapper for callables.
+    """
 
     __slots__ = ('__wrapped__', '__wrapper__', '__factory__')
 
-    def __init__(self, func, decorator, factory=None):
+    def __init__(self, func, decorator=None, factory=None):
         self.__wrapped__ = func
         self.__wrapper__ = decorator
         self.__factory__ = factory or getattr(decorator, '__self__', None)
 
     def __repr__(self):
         from recipes.pprint import callers
+
         return (f'<{type(self).__name__}('
                 f'{callers.describe(self.__wrapper__)}; '
                 f'wraps: {callers.pformat(self.__wrapped__)})>')
 
     def __call__(self, *args, **kws):
-        return self.__wrapper__(self.__wrapped__, *args, **kws)
+
+        if self.__wrapper__:
+            return self.__wrapper__(self.__wrapped__, *args, **kws)
+
+        return self.__wrapped__(*args, **kws)
 
     def __reduce__(self):
         return self.__factory__, (self.__wrapped__, False)
 
 
 class Factory:
+
+    __wrapper_class__ = Wrapper
 
     def __init__(self, *args, **kws):
         """
@@ -63,6 +72,7 @@ class Factory:
             The decorator
         """
         assert callable(func)
+        assert callable(wrapper)
 
         if emulate:
             # make the wrapper appear to be the original function. This should
@@ -85,8 +95,10 @@ class Factory:
             return decorator
 
         # decorator is explicitly a Wrapper object!
-        logger.debug('Initializing wrapper for: {}.', func)
-        return Wrapper(func, wrapper, self)
+        logger.debug('Initializing wrapper {} for: {}.',
+                     self.__wrapper_class__.__name__, func)
+
+        return self.__wrapper_class__(func, wrapper, self)
 
 
 # ---------------------------------------------------------------------------- #
