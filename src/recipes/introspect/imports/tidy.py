bc3d22be9b860419d1098aa0fcd7278f01f94e4b
astromancer <hannes@saao.ac.za>
2024-02-26 21:16:59 +0200

    DEV: import tidy additional ignore patterns from config/cli

--- src/recipes/introspect/imports/tidy.py
+++ src/recipes/introspect/imports/tidy.py
@@ -11,18 +11,23 @@ import numpy as np
 from loguru import logger
 
 # relative
+from ...config import ConfigNode
 from ...io.gitignore import GitIgnore
 from ...concurrency.executor import Executor
 from . import STYLES, refactor
 
 
 # ---------------------------------------------------------------------------- #
+CONFIG = ConfigNode.load_module(__file__)
+
+# ---------------------------------------------------------------------------- #
+
 
 def _py_file(path):
     return path.suffix == '.py'
 
 
-def _iter_files(file_or_folder, recurse):
+def _iter_files(file_or_folder, recurse, ignore):
 
     path = Path(file_or_folder).resolve()
 
@@ -30,9 +35,12 @@ def _iter_files(file_or_folder, recurse):
         logger.warning("File or directory does not exist: '{}'.", path)
         return
 
+    if (gitignore := path / '.gitignore').exists():
+        gitignore = GitIgnore(gitignore)
+        gitignore.add(ignore)
+
     if path.is_dir():
-        if (gitignore := path / '.gitignore').exists():
-            gitignore = GitIgnore(gitignore)
+        if gitignore:
             files = gitignore.iterdir(depth=(1, any)[recurse])
             files = filter(_py_file, files)
         else:
@@ -41,7 +49,9 @@ def _iter_files(file_or_folder, recurse):
         yield from files
 
     elif _py_file(path):
-        yield path
+        if not gitignore.match(str(path)):
+            logger.debug('Ignoring: {!r}.', str(path))
+            yield path
 
     else:
         logger.warning("Not a valid python file: '{}'.", path)
@@ -59,6 +69,13 @@ class Tidy(Executor):
         self.results = []
         self.style = str(style)
         self.recurse = bool(recurse)
+        self.ignore = list(CONFIG.get('ignore', ()))
+
+    def __call__(self, *args, ignore=(), **kws):
+        if ignore:
+            self.ignore.extend(ignore)
+
+        return super().__call__(*args, **kws)
 
     def compute(self, file, index, **kws):
         logger.info('Tidying import statements in {}.', repr(str(file)))
@@ -76,7 +93,7 @@ class Tidy(Executor):
     def _get_workload(self, files_or_folders):
         self.logger.info('Resolving workload.')
         for item in files_or_folders:
-            for file in _iter_files(item, self.recurse):
+            for file in _iter_files(item, self.recurse, self.ignore):
                 yield file
 
     def collect(self, index, result):
@@ -89,9 +106,10 @@ class Tidy(Executor):
 @click.argument('files_or_folders', nargs=-1)
 @click.option('-s', '--style', default='aesthetic', show_default=True, type=click.Choice(STYLES))
 @click.option('-r', '--recurse', default=True, show_default=True, type=click.BOOL)
+@click.option('-i', '--ignore', default=())
 @click.option('-j', '--njobs', default=-1, show_default=True, type=click.INT)
 # @click.option('-v', '--verbose', count=True)
-def main(files_or_folders, style, recurse, njobs):
+def main(files_or_folders, style, recurse, ignore, njobs):
     #   filter_unused=None,
     #   split=0,
     #   merge=1,
@@ -109,6 +127,6 @@ def main(files_or_folders, style, recurse, njobs):
     logger.configure(activation=[('recipes', 'INFO')])
 
     tidy = Tidy(style, recurse)
-    tidy(files_or_folders, njobs=njobs)
+    tidy(files_or_folders, ignore=ignore, njobs=njobs)
 
     return 0
