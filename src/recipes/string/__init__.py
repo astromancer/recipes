9349fb72b2d5c6c7aee2e25ade4d5b2d5e3bcb37
astromancer <hannes@saao.ac.za>
2024-03-13 18:09:33 +0200

    MNT: import duties

--- src/recipes/string/__init__.py
+++ src/recipes/string/__init__.py
@@ -3,13 +3,18 @@ Utilites for woking with strings.
 """
 
 from . import delimited
-from .utils import *
-from .casing import *
-from .affixes import *
-from .justify import *
-from .plurals import *
-from .stacking import *
 from .percentage import Percentage
+from .stacking import hstack, vstack, width
+from .justify import justify, overlay, resolve_justify
+from .plurals import naive_english_plural, named_items, numbered, pluralize
+from .casing import (camel_case, kebab_case, monospaced, pascal_case,
+                     snake_case, strike, title)
+from .utils import (backspaced, indent, insert, most_similar,
+                    partition_whitespace, similarity, strings, strip_non_ascii,
+                    sub, surround, truncate)
+from .affixes import (remove_affix, remove_prefix, remove_suffix,
+                      remove_suffixes, replace_prefix, replace_suffix,
+                      shared_affix, shared_prefix, shared_suffix)
 
 # alias
 delim = delimited
