72bd03c9e571c0138ef01f11625f08f6c77deddb
astromancer <hannes@saao.ac.za>
2024-03-25 22:53:14 +0200

    DEV: refactor `containers.dicts` module 🏭

--- src/recipes/containers/dicts/core.py
+++ src/recipes/containers/dicts/core.py
@@ -5,150 +5,27 @@ Recipes extending mutable mapping functionality.
 
 # std
 import numbers
-import itertools as itt
-import contextlib as ctx
-from pathlib import Path
 from collections import OrderedDict, UserDict, abc, defaultdict
 
 # relative
 from ...flow import Emit
-from ...iter import cofilter
-from ...pprint.mapping import PrettyPrint, pformat
+from ...pprint.mapping import PrettyPrint
 from ..ensure import is_scalar
 
 
 # ---------------------------------------------------------------------------- #
-# utils
 
-def is_dict(obj):
-    return isinstance(obj, abc.MutableMapping)
-
-
-# alias
-isdict = is_mapping = is_map = is_dict
-
-
-def dump(mapping, filename, **kws):
-    """
-    Write dict to file in human readable form
-
-    Parameters
-    ----------
-    mapping : [type]
-        [description]
-    filename : [type]
-        [description]
-    """
-    Path(filename).write_text(pformat(mapping, **kws))
-
-
-def invert(d, conversion=None):
-    if conversion is None:
-        conversion = {list: tuple}
-
-    inverted = type(d)()
-    for key, val in d.items():
-        kls = type(val)
-        if kls in conversion:
-            val = conversion[kls](val)
-
-        if not isinstance(val, abc.Hashable):
-            raise ValueError(
-                f'Cannot invert dictionary with non-hashable item: {val} of '
-                f'type {type(val)}. You may wish to pass a conversion mapping'
-                ' to this function to aid invertingof mappings that contain '
-                f'non-hashable items.'
-            )
-
-        inverted[val] = key
-    return inverted
-
-
-def groupby(func, items):
-    """
-    Group objects by function return value.
-
-    Parameters
-    ----------
-    func : callable
-        The group id function.
-    items : Iterable
-        Objects to be grouped.
-
-    Examples
-    --------
-    >>> groupby(str.isupper, 'abcDEF')
-    {False: ['a', 'b', 'c'], True: ['D', 'E', 'F']}
-
-    Returns
-    -------
-    dict[Any, list]
-        (group_id, items)
-    """
-    with ctx.suppress(TypeError):
-        items = sorted(items, key=func)
-    return {group: list(itr)
-            for group, itr in itt.groupby(items, func)}
-
-
-def merge(*mappings, **kws):
-    """
-    Merge an arbitrary number of dictionaries together by repeated update.
-
-    Examples
-    --------
-    >>> merge(*({f'{(l := case(letter))}': ord(l)}
-    ...        for case in (str.upper, str.lower) for letter in 'abc'),
-    ...       z=100)
-    {'A': 65, 'B': 66, 'C': 67, 'a': 97, 'b': 98, 'c': 99, 'z': 100}
-
-    Returns
-    -------
-    dict
-        Merged dictionary.
-    """
-
-    out = {}
-    for mapping in mappings:
-        out.update(mapping)
-    out.update(kws)
-    return out
-
-
-def filter(func_or_mapping, mapping=None):
-    func = func_or_mapping if mapping else None
-    mapping = (mapping or func_or_mapping)
-    new = zip(*cofilter(func, mapping.values(), mapping.keys())[::-1])
-
-    if isinstance(mapping, defaultdict):
-        return type(mapping)(mapping.default_factory, new)
-
-    return type(mapping)(new)
-
-
-def remove(mapping, keys, *extra):
-    # remove keys
-    split(mapping, keys, *extra)
-    return mapping
-
-
-def split(mapping, keys, *extra):
-    if isinstance(keys, str):
-        keys = keys,
-
-    keys = (*keys, *extra)
-    return mapping, dict(_split(mapping, keys))
-
-
-def _split(mapping, keys):
-    for key in keys:
-        if key in mapping:
-            yield key, mapping.pop(key)
-
+def factory(attrs='read', ordered=False, indexed=False, missing=None,
+            translate=()):
+    
+    # TODO: a factory function which takes requested props, eg:
+    bases = [_AccessManager]
+    if ordered:
+        bases.append()
+    
+    raise NotImplementedError
 
 # ---------------------------------------------------------------------------- #
-# TODO: a factory function which takes requested props, eg:
-# def factory(attrs='read-only', ordered=True, indexed=True):
 
 class Invertible:
     """
@@ -270,7 +147,7 @@ class AutoVivify(_AccessManager):
 
 
 # ---------------------------------------------------------------------------- #
-# TODO AttrItemWrite
+# TODO AttrReadWrite
 
 
 class AttrBase(dict):
@@ -413,63 +290,6 @@ class ListLike(Indexable, OrderedDict, PrettyPrint):
     def append(self, item):
         self.check_item(item)
         self[self.auto_key()] = self.convert_item(item)
-
-
-# class Indexable:
-#     """Item access through integer keys like list"""
-#
-#     def __missing__(self, key):
-#         if isinstance(key, int):
-#             l = len(self)
-#             assert -l <= key < l, 'Invalid index: %r' % key
-#             return self[list(self.keys())[key]]
-#         # if isinstance(key, slice)
-#         # cannot do slices here since the are not hashable
-#         return super().__missing__(key)
-
-
-# class ListLike(AttrReadItem, OrderedDict, Indexable):
-#     """
-#     Ordered dict with key access via attribute lookup. Also has some
-#     list-like functionality: indexing by int and appending new data.
-#     Best of both worlds.  Also make sure labels are always arrays.
-#     """
-#      = 'item'
-#
-#     def __init__(self, groups=None, **kws):
-#         # if we get a list / tuple try interpret as list of arrays (group
-#         # labels)
-#         if groups is None:
-#             super().__init__()
-#         elif isinstance(groups, (list, tuple)):
-#             super().__init__()
-#             for i, item in enumerate(groups):
-#                 self[self.auto_key()] = self.convert_item(item)
-#         else:
-#             super().__init__(groups, **kws)
-#
-#     def __setitem__(self, key, item):
-#         item = self.convert_item(item)
-#         OrderedDict.__setitem__(self, key, item)
-#
-#     def convert_item(self, item):
-#         return np.array(item, int)
-#
-#     def auto_key(self):
-#         return 'group%i' % len(self)
-#
-#     def append(self, item):
-#         self[self.auto_key()] = self.convert_item(item)
-#
-#     # def rename(self, group_index, name):
-#     #     self[name] = self.pop(group_index)
-#
-#     @property
-#     def sizes(self):
-#         return [len(labels) for labels in self.values()]
-#
-#     def inverse(self):
-#         return {lbl: gid for gid, labels in self.items() for lbl in labels}
 
 
 class Record(Indexable, OrderedAttrDict):
@@ -675,3 +495,61 @@ class DefaultOrderedDict(OrderedDict):
 
 # alias
 OrderedDefaultDict = DefaultOrderedDict
+
+
+
+# class Indexable:
+#     """Item access through integer keys like list"""
+#
+#     def __missing__(self, key):
+#         if isinstance(key, int):
+#             l = len(self)
+#             assert -l <= key < l, 'Invalid index: %r' % key
+#             return self[list(self.keys())[key]]
+#         # if isinstance(key, slice)
+#         # cannot do slices here since the are not hashable
+#         return super().__missing__(key)
+
+
+# class ListLike(AttrReadItem, OrderedDict, Indexable):
+#     """
+#     Ordered dict with key access via attribute lookup. Also has some
+#     list-like functionality: indexing by int and appending new data.
+#     Best of both worlds.  Also make sure labels are always arrays.
+#     """
+#      = 'item'
+#
+#     def __init__(self, groups=None, **kws):
+#         # if we get a list / tuple try interpret as list of arrays (group
+#         # labels)
+#         if groups is None:
+#             super().__init__()
+#         elif isinstance(groups, (list, tuple)):
+#             super().__init__()
+#             for i, item in enumerate(groups):
+#                 self[self.auto_key()] = self.convert_item(item)
+#         else:
+#             super().__init__(groups, **kws)
+#
+#     def __setitem__(self, key, item):
+#         item = self.convert_item(item)
+#         OrderedDict.__setitem__(self, key, item)
+#
+#     def convert_item(self, item):
+#         return np.array(item, int)
+#
+#     def auto_key(self):
+#         return 'group%i' % len(self)
+#
+#     def append(self, item):
+#         self[self.auto_key()] = self.convert_item(item)
+#
+#     # def rename(self, group_index, name):
+#     #     self[name] = self.pop(group_index)
+#
+#     @property
+#     def sizes(self):
+#         return [len(labels) for labels in self.values()]
+#
+#     def inverse(self):
+#         return {lbl: gid for gid, labels in self.items() for lbl in labels}
