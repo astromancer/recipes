1ef7af1fd7904cd99e6b1fe98c5c545c7cf2acdd
astromancer <hannes@saao.ac.za>
2024-10-02 15:21:15 +0200

    DEV: namespace formatter gains `remap` parameter

--- src/recipes/pprint/namespace.py
+++ src/recipes/pprint/namespace.py
@@ -7,24 +7,64 @@ from .. import op
 from .mapping import pformat as _pformat
 
 
-def pformat(target, attrs=..., maybe=(), ignore='*_', name=None, rename=(),
-            enclose='<>', **kws):
+def pformat(target, attrs=..., maybe=(), ignore='*_', rename=(), remap=(),
+            name=None, enclose='<>', **kws):
+    """
+    Pretty format an object's namespace (attributes, properties and their values) 
+
+    Parameters
+    ----------
+    target : object
+        The instance whose namespace will be represented.
+    attrs : Sequence[str|ellipsis] or ellipsis, optional
+        The attribute names. The default value, `...`, means use all available 
+        attributes in `__slots__` or `__dict__`. To represent additional 
+        properties, you can include more names with the ellipsis in a sequence
+        eg: (..., 'aproperty'). This is interpreted as though the slot names
+        are expanded in-place at the position of the elipsis, so order matters.
+    maybe : Sequence[str], optional
+        Attribute or property names to show conditionally based on the value
+        of said attribute. By default ().
+    ignore : str, optional
+        Pattern(s) of attribute names to ignore. The default value, '*_',
+        ignores all names beginning with an underscore.
+    rename : dict[str], optional
+        Mapping of attributes (str) that will be renamed to new names, by
+        default ().  Values are kept unchanged.
+    remap : dict[str], optional
+        Attributes (str) whose value will be replaced by the value of a
+        different attribute, by default (). Attribute names are kept unchanged.
+    name : str, optional
+        Object name to use. The default, None, uses the object class name.
+    enclose : str, optional
+        Characters used to enclose the entire representation string. The
+        default, '<>', emulates results from the builtin `repr`.
+
+
+    Returns
+    -------
+    str
+        Object namespace representation.
+    """
 
     name = name or type(target).__name__
+
+    if remap := dict(remap):
+        attrs = op.resolve_attr_names(target, attrs, ignore)
+        for old, new in remap.items():
+            attrs[attrs.index(old)] = new
+
+    # fetch attribute values
     state = op.get.attrs(target, attrs, maybe, ignore)
 
-    if rename := dict(rename):
+    if rename := {**dict(rename), **dict(zip(remap.values(), remap.keys()))}:
         state = {rename.get(key, key): val for key, val in state.items()}
 
     opn, *close = enclose
-    newline = kws.get('newline', '')
-    if '\n' in newline:
-        space = len(opn) + len(kws.get('brackets', '()')[0])
-        kws['newline'] = ' ' * space + newline
-
-    return ''.join((opn,
-                    _pformat(state, name, **kws),
-                    *close))
+    # if '\n' in (newline := kws.get('newline', '')):
+    #     space = len(opn) + len(kws.get('brackets', '()')[0])
+    #     kws['newline'] = newline + ' ' * space
+    return ''.join((_pformat(state, f'{opn}{name}', **kws), *close))
 
 
 # ---------------------------------------------------------------------------- #
